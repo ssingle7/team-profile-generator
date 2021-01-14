394716a0a8d72ed1e63249493f1ea475e33860a5
Selena Singleton <ssingle7@gmail.com>
2021-01-14 18:42:04 -0500

    added the inquirer package

--- app.js
+++ app.js
@@ -95,6 +95,10 @@ Intern: [{
 }, 
 ]
 
+//->RUN APP-> 
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
